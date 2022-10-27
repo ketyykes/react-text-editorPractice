8a2c1349d9eb5062bf27b12f71a80d6bdd0b0d24
ketyykes <xi4869@gmail.com>
2022-10-27 23:34:44 +0800

    TiptapHelloWorld

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-//載入react需要用的依賴項
-import React, { useState } from 'react'
-// 引入slate的編輯工廠
-import { createEditor } from 'slate'
-//引入slate的component和react的plugin
-import { Slate, Editable, withReact } from 'slate-react'
-function App() {
-  const [editor] = useState(() => withReact(createEditor()))
-  const initialValue = [{
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },]
+import React from 'react'
+import Tiptap from './Tiptap'
 
-
-  //建立一個Slate component就像提供了一個context面對底下每個component可以共享value
-  //添加onKeyDown的事件處理器能像使用react組件一樣
+const App = () => {
   return (
-    <Slate editor={editor} value={initialValue}>
-      <Editable onKeyDown={(event) => {
-        console.log(event.key);
-      }}
-      />
-    </Slate>
+    <div>
+      <Tiptap />
+    </div>
   )
 }
 
